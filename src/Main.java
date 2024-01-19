0bf74d5500bebcc2a21b14aa52ef2ecea32ccaf8
AlexDv <DvoinikovAV@yandex.ru>
2024-01-19 17:01:11 +0300

    Это 0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        byte numOs=1;
+        if (numOs!=0&&numOs!=1)
+        {
+            System.out.println("Введите корректный номер ОС");
+        }
+        else if (numOs==0)
+        {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else
+        {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
     }
 }
