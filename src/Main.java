90c217d640793588f2791522fc541e2c0985dde8
AlexDv <DvoinikovAV@yandex.ru>
2024-01-21 02:12:33 +0300

    Домашняя работа 5 задание 5

--- src/Main.java
+++ src/Main.java
@@ -1,84 +1,62 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         //Задание 1
-        byte numOs=1;
-        if (numOs!=0&&numOs!=1)
-        {
+        byte numOs = 1;
+        if (numOs != 0 && numOs != 1) {
             System.out.println("Введите корректный номер ОС");
-        }
-        else if (numOs==0)
-        {
+        } else if (numOs == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        else
-        {
+        } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
         //Задание 2
-        short agePr=2016;
-        numOs=0;
-        if(numOs==0&&agePr<2015)
-        {
+        short agePr = 2016;
+        numOs = 0;
+        if (numOs == 0 && agePr < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }
-        else if(numOs==0&&agePr>=2015)
-        {
+        } else if (numOs == 0 && agePr >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
-        if(numOs==1&&agePr<2015)
-        {
+        if (numOs == 1 && agePr < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }
-        else if(numOs==1&&agePr>=2015)
-        {
+        } else if (numOs == 1 && agePr >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
-        if (numOs!=0&&numOs!=1)
-        {
+        if (numOs != 0 && numOs != 1) {
             System.out.println("Введите корректный номер ОС");
         }
 
         //Задание 3
-        short god=1588;
+        short god = 1588;
 
-        if((((god%4)==0&&(god%100)!=0)||(god%400)==0)&&god>1584)
-        {
+        if ((((god % 4) == 0 && (god % 100) != 0) || (god % 400) == 0) && god > 1584) {
             System.out.println("Этот год високосный");
-        }
-        else
-        {
+        } else {
             System.out.println("Этот год не високосный");
 
         }
 
         //Задание 4
-        byte deliveryDistance=101;
-        byte srok=0;
-        if (deliveryDistance>100) {
+        byte deliveryDistance = 101;
+        byte srok = 0;
+        if (deliveryDistance > 100) {
             System.out.println("Доставки нет");
-        }
-        else
-        {
-            if (deliveryDistance <= 20)
-            {
+        } else {
+            if (deliveryDistance <= 20) {
                 srok = 1;
             }
-            if (deliveryDistance > 20 && deliveryDistance < 60)
-            {
+            if (deliveryDistance > 20 && deliveryDistance < 60) {
                 srok = 2;
             }
-            if (deliveryDistance >= 60 && deliveryDistance <= 100)
-            {
+            if (deliveryDistance >= 60 && deliveryDistance <= 100) {
                 srok = 3;
             }
-            System.out.println("Потребуется дней: "+srok);
+            System.out.println("Потребуется дней: " + srok);
         }
         //Задание 5
-        byte monthNumber = 12;
-        switch (monthNumber){
+        byte monthNumber = 15;
+        switch (monthNumber) {
             case 1:
                 System.out.println("Это зима");
                 break;
@@ -120,14 +98,7 @@ public class Main
         }
 
 
-
-
-
-
-
-
     }
-
 
 
 }
