3acd63848889f5734f64fbe9792fd9313044ac49
AlexDv <DvoinikovAV@yandex.ru>
2024-01-20 10:23:12 +0300

    Домашняя работа 5 задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
+        //Задание 1
         byte numOs=1;
         if (numOs!=0&&numOs!=1)
         {
@@ -15,6 +17,36 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
+        //Задание 2
+        short agePr=2016;
+        numOs=0;
+        if(numOs==0&&agePr<2015)
+        {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        else if(numOs==0&&agePr>=2015)
+        {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if(numOs==1&&agePr<2015)
+        {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        else if(numOs==1&&agePr>=2015)
+        {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        if (numOs!=0&&numOs!=1)
+        {
+            System.out.println("Введите корректный номер ОС");
+        }
+
+
+
 
     }
+
+
+
 }
+
